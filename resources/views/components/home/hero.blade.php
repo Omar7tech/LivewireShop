aa6039c90e7d96bc1a3b0fdcde11a61587288705
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-06 10:45:21 +0300

    asd

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,9 +1,9 @@
 <div class="h-screen bg-white flex items-center justify-center sm:p-0 md:p-[1%] box-border transition-all duration-300">
   <div class="h-full w-full sm:rounded-none md:rounded-lg bg-cover bg-center transition-all duration-300"
     style="background-image: url('{{ asset('covers/home-hero.jpg') }}');">
-    <div id="content" class="mt-30 px-10">
-      <h1 class="text-6xl text-white font-extralight">
-        Design Your <span class="font-serif italic">Space</span>, Your Way
+    <div id="content" class="px-10 flex items-center justify-center w-full h-full">
+      <h1 class="text-6xl text-white font-extralight -translate-y-16">
+        Design Your <span class="font-playfair-display italic"> Space </span>, Your Way
       </h1>
     </div>
 
